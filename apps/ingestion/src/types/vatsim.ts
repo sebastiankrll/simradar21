b310bb1ab953513693dd664749e9ceadaceb294a
Sebastian Krüll <51778048+sebastiankrll@users.noreply.github.com>
2025-11-12 19:54:41 +0100

    Add vatsim data types

--- apps/ingestion/src/types/vatsim.ts
+++ apps/ingestion/src/types/vatsim.ts
@@ -1,14 +1,14 @@
 export interface VatsimData {
     general: VatsimGeneral;
-    pilots: any[];
-    controllers: any[];
-    atis: any[];
-    servers: any[];
-    prefiles: any[];
-    facilities: any[];
-    ratings: any[];
-    pilot_ratings: any[];
-    vatsim_special_centers: any[];
+    pilots: VatsimPilot[];
+    controllers: VatsimController[];
+    atis: VatsimATIS[];
+    servers: VatsimServers[];
+    prefiles: VatsimPrefile[];
+    facilities: VatsimMeta[];
+    ratings: VatsimMeta[];
+    pilot_ratings: VatsimMetaName[];
+    military_ratings: VatsimMetaName[];
 }
 
 interface VatsimGeneral {
@@ -16,4 +16,92 @@ interface VatsimGeneral {
     update_timestamp: string;
     connected_clients: number;
     unique_users: number;
+}
+
+interface VatsimPilot {
+    cid: number;
+    name: string;
+    callsign: string;
+    server: string;
+    pilot_rating: number;
+    military_rating: number;
+    latitude: number;
+    longitude: number;
+    altitude: number;
+    groundspeed: number;
+    transponder: string;
+    heading: number;
+    qnh_i_hg: number;
+    qnh_mb: number;
+    flight_plan?: VatsimPilotFlightPlan;
+    logon_time: string;
+    last_updated: string;
+}
+
+type VatsimPilotFlightPlan = {
+    flight_rules: 'I' | 'V' | 'S';
+    aircraft: string;
+    aircraft_faa: string;
+    aircraft_short: string;
+    departure: string;
+    cruise_tas: string;
+    altitude: string;
+    arrival: string;
+    alternate: string;
+    deptime: string;
+    enroute_time: string;
+    fuel_time: string;
+    remarks: string;
+    route: string;
+    revision_id: number;
+    assigned_transponder: string;
+}
+
+interface VatsimController {
+    cid: number;
+    name: string;
+    callsign: string;
+    frequency: string;
+    facility: number;
+    rating: number;
+    server: string;
+    visual_range: number;
+    text_atis: string[] | null;
+    last_updated: string;
+    logon_time: string;
+}
+
+interface VatsimATIS extends VatsimController {
+    atis_code: string;
+}
+
+interface VatsimServers {
+    ident: string;
+    hostname_or_ip: string;
+    location: string;
+    name: string;
+    client_connections_allowed: boolean;
+    is_sweatbox: boolean;
+}
+
+interface VatsimPrefile {
+    cid: number;
+    name: string;
+    callsign: string;
+    flight_plan: VatsimPilotFlightPlan;
+    last_updated: string;
+}
+
+interface VatsimMetaBase {
+    id: number;
+}
+
+interface VatsimMeta extends VatsimMetaBase {
+    short: string;
+    long: string;
+}
+
+interface VatsimMetaName extends VatsimMetaBase {
+    short_name: string;
+    long_name: string;
 }
