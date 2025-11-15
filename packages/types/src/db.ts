00c1eda8535adcde69778bf94b69701d7925dfe9
Sebastian Krüll <51778048+sebastiankrll@users.noreply.github.com>
2025-11-15 15:39:45 +0100

    Add shared types package

---
+++ packages/types/src/db.ts
@@ -0,0 +1,21 @@
+export interface OurAirportsCsv {
+    id: string;
+    ident: string;
+    type: string;
+    name: string;
+    latitude_deg: string;
+    longitude_deg: string;
+    elevation_ft: string;
+    continent: string;
+    iso_country: string;
+    iso_region: string;
+    municipality: string;
+    scheduled_service: string;
+    icao_code: string;
+    iata_code: string;
+    gps_code: string;
+    local_code: string;
+    home_link: string;
+    wikipedia_link: string;
+    keywords: string;
+}
