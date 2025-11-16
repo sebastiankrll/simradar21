8a075876722685fd6bc6e05d54aaba617648b197
Sebastian Krüll <51778048+sebastiankrll@users.noreply.github.com>
2025-11-16 02:16:13 +0100

    Add header component

---
+++ apps/web/components/Header/components/Clock.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { useEffect, useState } from "react"
+
+function getTime(time: Date): string {
+    const hours = time.getUTCHours()
+    const minutes = time.getUTCMinutes()
+
+    const formattedHours = String(hours).padStart(2, '0')
+    const formattedMinutes = String(minutes).padStart(2, '0')
+
+    return `${formattedHours}:${formattedMinutes}`
+}
+
+export default function Clock() {
+    const [time, setTime] = useState(new Date())
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date())
+        }, 1000)
+
+        return () => {
+            clearInterval(interval)
+        }
+    }, [])
+
+    return (
+        <div id="header-clock">
+            {getTime(time)}<span>UTC</span>
+        </div>
+    )
+
+}
---
+++ apps/web/components/Header/components/Search.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { useEffect, useState } from "react"
+
+export default function Search({ }) {
+    const [searchValue, setSearchValue] = useState('')
+
+    useEffect(() => {
+        const resetInput = () => {
+            setSearchValue('')
+        }
+
+        document.addEventListener('click', resetInput)
+
+        return () => {
+            document.removeEventListener('click', resetInput)
+        }
+    }, [])
+
+    return (
+        <input
+            id="header-search"
+            type="text"
+            placeholder="Not implemented yet."
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+        />
+    )
+}
