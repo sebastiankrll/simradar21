bff2ceb00a523347e76f84ec504670d02a8d5f0e
Sebastian Krüll <51778048+sebastiankrll@users.noreply.github.com>
2025-12-07 22:28:58 +0100

    Update DB env var handling and build process

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -9,6 +9,8 @@ export default defineConfig({
 		path: "prisma/migrations",
 	},
 	datasource: {
-		url: `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}?schema=public`,
+		url:
+			process.env.POSTGRES_URL ||
+			`postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}?schema=public`,
 	},
 });
